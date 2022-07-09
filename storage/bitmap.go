9c8276055244d59c76eb9849cba26355aef12354
acver <1246009411@qq.com>
2022-07-10 00:45:53 +0800

    添加非线程安全位图,添加benchmark测试用例

--- storage/bitmap.go
+++ storage/bitmap.go
@@ -2,31 +2,41 @@ package storage
 
 import "sync"
 
-/**
-基于uint64数组实现的线程安全的位图
-*/
-
-type BitMap struct {
-	rwLocks         []sync.RWMutex
-	bitMapData      []uint64
-	lockGranularity uint64
+type BitMap interface {
+	Get(uint64) bool
+	Set(uint64, bool)
 }
 
-func NewBitMap(size uint64, lock_granularity uint64) *BitMap {
-	bitMapDataLen := (size-1)/64 + 1
-	rwLocksLen := (size-1)/lock_granularity + 1
+func NewBitMap(size uint64, lock_granularity uint64) BitMap {
+	bitMapDataLen := (size-1)>>6 + 1
+	if lock_granularity == 0 {
+		return &bitMap{
+			bitMapData: make([]uint64, bitMapDataLen),
+		}
+	}
 
-	return &BitMap{
+	rwLocksLen := (size-1)/lock_granularity + 1
+	return &threadSafeBitMap{
 		rwLocks:         make([]sync.RWMutex, rwLocksLen),
 		bitMapData:      make([]uint64, bitMapDataLen),
 		lockGranularity: lock_granularity,
 	}
 }
 
-func (b *BitMap) Get(pos uint64) bool {
-	idx := pos / 64
+/**
+基于uint64数组实现的线程安全的位图
+*/
+
+type threadSafeBitMap struct {
+	rwLocks         []sync.RWMutex
+	bitMapData      []uint64
+	lockGranularity uint64
+}
+
+func (b *threadSafeBitMap) Get(pos uint64) bool {
+	idx := pos >> 6
 	var mask uint64 = 1
-	mask = mask << (pos % 64)
+	mask = mask << (pos & 63)
 
 	lock_idx := pos / b.lockGranularity
 
@@ -38,16 +48,44 @@ func (b *BitMap) Get(pos uint64) bool {
 	return (b.bitMapData[idx] & mask) != 0
 }
 
-func (b *BitMap) Set(pos uint64, value bool) {
-	idx := pos / 64
+func (b *threadSafeBitMap) Set(pos uint64, value bool) {
+	idx := pos >> 6
 	var mask uint64 = 1
-	mask = mask << (pos % 64)
+	mask = mask << (pos & 63)
 
 	lock_idx := pos / b.lockGranularity
 	b.rwLocks[lock_idx].Lock()
 	defer func() {
 		b.rwLocks[lock_idx].Unlock()
 	}()
+
+	if value == true {
+		b.bitMapData[idx] = b.bitMapData[idx] | mask
+	} else {
+		b.bitMapData[idx] = b.bitMapData[idx] & ^mask
+	}
+}
+
+/**
+基于uint64的位图
+*/
+
+type bitMap struct {
+	bitMapData []uint64
+}
+
+func (b *bitMap) Get(pos uint64) bool {
+	idx := pos >> 6
+	var mask uint64 = 1
+	mask = mask << (pos & 63)
+
+	return (b.bitMapData[idx] & mask) != 0
+}
+
+func (b *bitMap) Set(pos uint64, value bool) {
+	idx := pos >> 6
+	var mask uint64 = 1
+	mask = mask << (pos & 63)
 
 	if value == true {
 		b.bitMapData[idx] = b.bitMapData[idx] | mask
--- storage/bitmap_test.go
+++ storage/bitmap_test.go
@@ -1,10 +1,12 @@
 package storage
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
-func TestBitMap(t *testing.T) {
+func TestThreadSafeBitMap(t *testing.T) {
 	type fields struct {
 		size            uint64
 		lockGranularity uint64
@@ -95,5 +97,144 @@ func TestBitMap(t *testing.T) {
 				}
 			}
 		})
+	}
+}
+
+func TestBitMap(t *testing.T) {
+	type fields struct {
+		size            uint64
+		lockGranularity uint64
+	}
+	type getArgs struct {
+		pos     uint64
+		wantRet bool
+	}
+	type setArgs struct {
+		pos   uint64
+		value bool
+	}
+	type args struct {
+		set []setArgs
+		get []getArgs
+	}
+	tests := []struct {
+		name   string
+		race   bool
+		fields fields
+		args   args
+	}{
+		{
+			name: "简单测试",
+			race: false,
+			fields: fields{
+				size:            1000000,
+				lockGranularity: 0,
+			},
+			args: args{
+				set: []setArgs{
+					{15, true},
+					{25, true},
+					{80, true},
+					{999999, true},
+					{80, false},
+				},
+				get: []getArgs{
+					{15, true},
+					{25, true},
+					{80, false},
+					{999999, true},
+				},
+			},
+		},
+		{
+			name: "简单测试2",
+			race: true,
+			fields: fields{
+				size:            1000000,
+				lockGranularity: 0,
+			},
+			args: args{
+				set: []setArgs{
+					{80, true},
+					{999999, true},
+					{999998, true},
+					{80, false},
+				},
+				get: []getArgs{
+					{80, false},
+					{999999, true},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := NewBitMap(tt.fields.size, tt.fields.lockGranularity)
+			if tt.race {
+				for _, set := range tt.args.set {
+					set := set
+					go func() {
+						b.Set(set.pos, set.value)
+					}()
+				}
+			} else {
+				for _, set := range tt.args.set {
+					set := set
+					b.Set(set.pos, set.value)
+				}
+			}
+
+			for _, get := range tt.args.get {
+				ret := b.Get(get.pos)
+				if !tt.race && ret != get.wantRet {
+					t.Errorf("pos %v want %v got %v\n", get.pos, get.wantRet, ret)
+				}
+			}
+		})
+	}
+}
+
+func generateu64(n int) []uint64 {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]uint64, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, uint64(rand.Int63n(10000000)))
+	}
+	return nums
+}
+func generate(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Intn(10000000))
+	}
+	return nums
+}
+
+func BenchmarkThreadSafeBitMap(b *testing.B) {
+	set_arr := generateu64(10000000)
+	get_arr := generateu64(10000000)
+	for n := 0; n < b.N; n++ {
+		bm := NewBitMap(10000000, 1000)
+		for _, set_val := range set_arr {
+			bm.Set(set_val, true)
+		}
+		for _, get_val := range get_arr {
+			bm.Get(get_val)
+		}
+	}
+}
+
+func BenchmarkBitMap(b *testing.B) {
+	set_arr := generateu64(10000000)
+	get_arr := generateu64(10000000)
+	for n := 0; n < b.N; n++ {
+		bm := NewBitMap(10000000, 0)
+		for _, set_val := range set_arr {
+			bm.Set(set_val, true)
+		}
+		for _, get_val := range get_arr {
+			bm.Get(get_val)
+		}
 	}
 }
